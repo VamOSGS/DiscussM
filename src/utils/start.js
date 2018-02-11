7480834c59e3f673c8c736a7c1695b20730bf4bb
VamOSGS <vamosgs2001@gmail.com>
2018-02-11 11:10:03 +0400

    PORT fix

--- src/utils/start.js
+++ src/utils/start.js
@@ -3,5 +3,5 @@ dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 const { PORT, HOST } = process.env;
 
 export default function(app) {
-    app.listen(PORT, () => console.log(`started in http://${HOST}:${PORT}`));
+    app.listen(PORT || 8000, () => console.log(`started in http://${HOST}:${PORT}`));
 }
